1bbf87ca25b9614fdb54c5e6bf921068d50dcd58
kunzhao3 <2278478075@qq.com>
2017-12-21 13:48:03 +0800

    添加json库

--- switchSQL/src/test/java/com/gome/service/TestGome.java
+++ switchSQL/src/test/java/com/gome/service/TestGome.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.alibaba.fastjson.JSON;
 import com.gome.dao.sqltest.MultipleDataSource;
 import com.gome.modle.TmAppCaseInfo;
 
@@ -19,7 +20,9 @@ public class TestGome {
 	public void test(){
 		MultipleDataSource.setDataSourceKey("mySqlDataSource");
 		TmAppCaseInfo t1=tmAppCaseInfoService.getInfo54();
+		System.out.println("54数据库的数据"+JSON.toJSON(t1));
 		MultipleDataSource.setDataSourceKey("sqlServerDataSource");
 		TmAppCaseInfo t2=tmAppCaseInfoService.getInfo55();
+		System.out.println("55数据库的数据"+JSON.toJSON(t2));
 	}
 }
